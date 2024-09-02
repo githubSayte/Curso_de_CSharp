045682b4ec9de471eac7902ffd167a9ff10ce530
Hokague <criativasayte@gmail.com>
2024-09-02 11:41:14 -0300

    up 18

--- CursoCSaharp/Program.cs
+++ CursoCSaharp/Program.cs
@@ -3,22 +3,61 @@ using CursoCSharp;
 
 
 {
-   /**
+    /**
 
-    var explicacaoEnum = new ExplicacaoEnum();
+     var explicacaoEnum = new ExplicacaoEnum();
 
-    //chamando 
-    explicacaoEnum.DemonstrarEnum();
+     //chamando 
+     explicacaoEnum.DemonstrarEnum();
 
-    //Exibindo 
-    explicacaoEnum.ExplicacaoDetalhada();
-   */
+     //Exibindo 
+     explicacaoEnum.ExplicacaoDetalhada();
+    */
 
-    var recursao = new Recursao();
-    recursao.DemonstrarRecursao();
+    /**
+var recursao = new Recursao();
+recursao.DemonstrarRecursao();
+     */
+
+
+    /**
+var explicacao = new ExplicacaoStructs();
+explicacao.DemonstrarDiferenca();
+*/
+
+
+    /**
+// Criando uma instância da classe NullableTypesDemo
+var demo = new NullableTypesDemo();
+
+// Definindo valores para os tipos nullable
+demo.Numero = null; // Numero pode ser null
+demo.EstaCompleto = true; // EstaCompleto pode ser null ou bool
+
+// Exibindo os valores
+demo.ExibirValores();
+
+// Mostrando o uso do operador de coalescência nula
+Console.WriteLine($"Valor de Numero ou padrão: {demo.ObterValorOuPadrao()}");
+
+     // Explicando os conceitos
+     demo.ExplicarConceitos();
+*/
 
 
 
+ Colecoes colecoes = new Colecoes();
+
+ colecoes.ExplicarDictionary();
+ Console.WriteLine();
+
+ colecoes.ExplicarQueue();
+ Console.WriteLine();
+
+ colecoes.ExplicarStack();
+ Console.WriteLine();
+
+ colecoes.ExplicarSet();
 
 
 
--- CursoCSaharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ CursoCSaharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f95822f9d12900592026a7da0b4a44acf790cd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c957c9311d29162963d3b5334d6d9d4afbabc20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
